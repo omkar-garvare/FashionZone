76b8a4fc819e7a80f4a3766eeaed44cfc1a0231c
Omkar Garvare <omkargarvare@gmail.com>
2024-03-08 22:57:21 +0530

    initial commit

---
+++ includes/common.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "lifestylestore")or die($mysqli_error($con));
+session_start();
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 8605298576</p>	
+        </center>
+    </div>
+</footer>
